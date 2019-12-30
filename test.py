faae7d8c41eb080d508faff895636543e5b557f7
krinalbhadiyadra <59196201+krinalbhadiyadra@users.noreply.github.com>
2019-12-30 15:05:08 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,31 @@
+    allocate_lead(leads,config):
+    leads_list = [{"user_id":1, "size:500", "HQ":"US"},
+                    {"user_id":2, "size:700", "HQ":"US"}
+                    {"user_id":3, "size:50", "HQ":"CANADA"}
+                ]
+    User_A[0,500]
+    User_B[0,X]
+    User_c[500,X]
+    User_D
+
+
+class Graph: 
+  
+    def __init__(self, user, category, hq): 
+        self.USER = user 
+        self.CATEGORY = category 
+        self.HQ = HQ
+        
+    def allocate_lead(self, leads, config): 
+        
+        return (i >= 0 and i < self.ROW and 
+                j >= 0 and j < self.COL and 
+                not visited[i][j] and self.graph[i][j] == 1) 
+              
+  
+
+
+                ]
+
+
+
